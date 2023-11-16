c24ca0a4e7e94f41a7b4696ead98b9b2d6685cfd
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-11-16 19:49:32 +0900

    (2023.11.16) SOLID 원칙 2

--- Program/Program.cs
+++ Program/Program.cs
@@ -17,15 +17,48 @@ namespace Program
             #endregion
 
             #region OCP 5대 원칙
-            Marine marine = new Marine();
-            Ghost ghost = new Ghost();
-            Firebet firebet = new Firebet();
+            //Marine marine = new Marine();
+            //Ghost ghost = new Ghost();
+            //Firebet firebet = new Firebet();
 
-            UnitManager unitManager = new UnitManager();
+            //UnitManager unitManager = new UnitManager();
 
-            unitManager.Commend(marine);
-            unitManager.Commend(ghost);
-            unitManager.Commend(firebet);
+            //unitManager.Commend(marine);
+            //unitManager.Commend(ghost);
+            //unitManager.Commend(firebet);
+            #endregion
+
+            #region ISP 5대 원칙
+            //Wraith wraith = new Wraith();
+
+            //wraith.Attack();
+            //wraith.Move();
+            //wraith.Skil();
+
+            //BattleCruiser battleCruiser = new BattleCruiser();
+
+            //battleCruiser.Attack();
+            //battleCruiser.Move();
+            //battleCruiser.Skil();
+
+            //Valkyrie valkyrie = new Valkyrie();
+
+            //valkyrie.Attack();
+            //valkyrie.Move();
+
+            //DropShip dropShip = new DropShip();
+
+            //dropShip.Move();
+            //dropShip.Skil();
+            #endregion
+
+            #region DIP 5대 원칙
+
+            Character character = new Character(100, new Knife());
+
+            character.ChangeWeapon(new Axe());
+            character.ChangeWeapon(new Rifle());
+
             #endregion
         }
     }      
