b941f5180f6f3fa582c504234917d2938ea905ca
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-11-23 19:47:07 +0900

    (2023.11.23) 퀵 정렬

--- Program/Program.cs
+++ Program/Program.cs
@@ -6,57 +6,33 @@ namespace Program
 {
     internal class Program
     {
-        class BinarySearch
+        static void QuickSort(int[ ] array, int left, int right)
         {
-            // 이진 탐색 메소드
-            static int BinarySearchFunc(int[] arr, int target)
+            // 1. left가 right 크거나 같아졌을 때
+            if(left >= right)
             {
-                int left = 0;
-                int right = arr.Length - 1;
-
-                while (left <= right)
-                {
-                    int pivot = left + (right - left) / 2;
-
-                    // 찾고자 하는 값이 pivot과 같으면 인덱스 반환
-                    if (arr[pivot] == target)
-                        return pivot;
-
-                    // 찾고자 하는 값이 pivot보다 작으면 왼쪽 부분 탐색
-                    if (arr[pivot] > target)
-                        right = pivot - 1;
-                    else // 찾고자 하는 값이 pivot보다 크면 오른쪽 부분 탐색
-                        left = pivot + 1;
-                }
-
-                // 찾고자 하는 값이 배열에 없으면 -1 반환
-                return -1;
+                return;
             }
+        }
 
-            static void Main(string[] args)
-            {
-                #region 이진 탐색
-                // 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘입니다.
+        static void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+            x = y;
+            y = temporary;
+        }
 
-                // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정합니다.
+        static void Main(string[] args)
+        {
+            #region 퀵 정렬
+            // 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고 한 쪽에는 기준점보다 작은 항목들이 위치하고 다른 쪽에는 기준점 보다 큰 항목들이 위치합니다.
 
-                // 2. [pivot]의 값이 찾고자 하는 요소가 같다면 검색완료
+            // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘입니다.
 
-                // 3. [pivot의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색합니다.]
+            // 시간복잡도 : O(log n)
 
-                // 4. [pivot의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색합니다.]
-
-                int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-                int targetValue = 12;
-
-                int result = BinarySearchFunc(array, targetValue);
-
-                if (result != -1)
-                    Console.WriteLine($"요소 {targetValue}의 인덱스는 {result}입니다.");
-                else
-                    Console.WriteLine($"배열에서 요소 {targetValue}을(를) 찾을 수 없습니다.");
-                #endregion
-            }
+            int[] array = new int[2] { 10, 20 };
+            #endregion
         }
     }
 }
