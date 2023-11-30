e1e7cc3a9bfeafdd0237a437d46db0cc4d243923
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-11-30 19:48:07 +0900

    (2023.11.30) 깊이 우선 탐색

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
+using System.ComponentModel.Design;
 using System.Drawing;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Program
 {
@@ -8,18 +10,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            #region BFS (너비 우선 탐색)
-            AdjacencyList adjacencyList = new AdjacencyList(7);
-
-            adjacencyList.InsertEdge(0, 1);
-            adjacencyList.InsertEdge(0, 2);
-            adjacencyList.InsertEdge(1, 3);
-            adjacencyList.InsertEdge(1, 4);
-            adjacencyList.InsertEdge(2, 5);
-            adjacencyList.InsertEdge(2, 6);
-
-            adjacencyList.BFS(0);
-            #endregion
+            
         }
     }
 }
